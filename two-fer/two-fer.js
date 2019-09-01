5dcd5c5c7c6908c2d6b676ec383b25bd754a3428
oliver-arr <oarroyo.developer@gmail.com>
2019-09-01 05:14:18 +0100

    refactored using default parameters

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,1 +1,6 @@
+/* first solution
 export const twoFer = name => (name ? `One for ${name}, one for me.`: `One for you, one for me.`);
+*/
+
+// Refactored using default parameters, after receieving feedback.
+export const twoFer = (name = 'you') => `One for ${name}, one for me.`;
