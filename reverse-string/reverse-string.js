6716c3b16e165af810fb6d4438f4cc6b902718bf
oliver-arr <oarroyo.developer@gmail.com>
2019-09-03 17:40:02 +0100

    implemented the reverse() functionality using a while loop

--- reverse-string/reverse-string.js
+++ reverse-string/reverse-string.js
@@ -1,1 +1,17 @@
+/* 
 export const reverseString = str => [...str].reverse().join('');
+*/
+
+// implemented my version of the in-built reverse() functionality
+export const reverseString = str => {
+  const strCharacters = [...str];
+  let strReversed = '';
+  let index = strCharacters.length - 1;
+
+  while(strReversed.length < strCharacters.length){
+    strReversed += `${strCharacters[index]}`;
+    index--;
+  }
+
+return strReversed;
+};
