40b092cc2c5086d9ac159850b2c8031bec653746
oliver-arr <oarroyo.developer@gmail.com>
2019-09-04 00:01:43 +0100

    4/6 tests passing

--- collatz-conjecture/collatz-conjecture.js
+++ collatz-conjecture/collatz-conjecture.js
@@ -1,3 +1,16 @@
 export const steps = (number) => {
-  console.log(number);
+  let stepCount = 0;
+
+  while(number > 1){
+    if(number % 2 === 0){
+      number /= 2;
+      stepCount++;
+    } else {
+      number *= 3;
+      number += 1;
+      stepCount++;
+    }
+  } 
+  console.log({number, stepCount});
+  return stepCount;
 };
