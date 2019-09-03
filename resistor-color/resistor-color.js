f47baadf332ed5f15f762cb6c8a40d85a6993b1b
oliver-arr <oarroyo.developer@gmail.com>
2019-09-03 09:25:23 +0100

    refactored first attempt after feedback

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,4 +1,8 @@
-// retrieve the colour index from the array of COLORS using the built in Array method findIndex, which takes a callback function.
+/* first attempt
 export const colorCode = color => COLORS.findIndex(element => element === color);
+*/
+
+// refactored first attempt
+export const colorCode = color => COLORS.indexOf(color);
 
 export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
