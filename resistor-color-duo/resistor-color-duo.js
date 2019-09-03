08fb0a738cd0a6125fe59e0dceefe05b0e91c593
oliver-arr <oarroyo.developer@gmail.com>
2019-09-03 14:55:17 +0100

    project setup

---
+++ resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const value = (colours) => {console.log(colours)};
