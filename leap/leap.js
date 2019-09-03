741a3f6638a649db3c4f7c5429643ba6c0ebc23c
oliver-arr <oarroyo.developer@gmail.com>
2019-09-03 09:16:30 +0100

    function setup test

--- leap/leap.js
+++ leap/leap.js
@@ -1,8 +1,13 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/*
+Given a year, report if it is a leap year.
 
-export const isLeap = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+on every year that is evenly divisible by 4
+  except every year that is evenly divisible by 100
+    unless the year is also evenly divisible by 400
+
+For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+*/
+
+export const isLeap = (year) => {console.log(year)};
