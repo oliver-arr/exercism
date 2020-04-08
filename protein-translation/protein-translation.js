8f80d8c71da2efa7e86192c06d6e30544b5e1aad
oliver-arr <oarroyo.developer@gmail.com>
2020-04-08 17:12:56 +0100

    Find a single codon that translates in to a protein

--- protein-translation/protein-translation.js
+++ protein-translation/protein-translation.js
@@ -1,3 +1,21 @@
-export const translate = (rna) => {
-  console.log(rna);
+export const translate = rna => {
+  let match = [];
+
+  const proteinTranslation = {
+    Methionine: ["AUG"],
+    Phenylalanine: ["UUU", "UUC"],
+    Leucine: ["UUA", "UUG"],
+    Serine: ["UCU", "UCC", "UCA", "UCG"],
+    Tyrosine: ["UAU", "UAC"],
+    Cysteine: ["UGU", "UGC"],
+    Tryptophan: ["UGG"],
+    STOP: ["UAA", "UAG", "UGA"]
+  };
+
+  Object.keys(proteinTranslation).forEach(protein => {
+    if (proteinTranslation[protein].includes(rna)) {
+      match.push(protein);
+    }
+  });
+  return match;
 };
