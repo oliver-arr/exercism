1800574d4eea41b6093726f510aa1f3174a68cf3
oliver-arr <oarroyo.developer@gmail.com>
2020-04-29 12:20:59 +0100

    first attempt: all tests passing

--- armstrong-numbers/armstrong-numbers.js
+++ armstrong-numbers/armstrong-numbers.js
@@ -1,3 +1,12 @@
 export const isArmstrongNumber = (num) => {
-  console.log(num);
+  const power = num.toString().split("").length;
+  const digits = num
+    .toString()
+    .split("")
+    .map((num) => Number(num));
+
+  const sum = digits.reduce((acc, curVal) => {
+    return acc + Math.pow(curVal, power);
+  }, 0);
+  return sum === num;
 };
